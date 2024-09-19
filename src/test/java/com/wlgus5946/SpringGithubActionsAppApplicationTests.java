22da9349ce4112422627f022c9fba7bd22e1564c
jungmayo <wlgus5946@gmail.com>
2024-09-19 15:28:02 +0900

    아마존 + github Action Test

---
+++ src/test/java/com/wlgus5946/SpringGithubActionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wlgus5946;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
